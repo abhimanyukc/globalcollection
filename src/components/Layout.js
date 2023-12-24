798791a0ec017ed0e32db2404b75242f2be72db8
Abhimanyu kc <abhidesign88@gmail.com>
2023-12-24 13:06:11 +0545

    finished setting up custom google font with tailwind.config.js and _app.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,16 @@
+import { Inter} from 'next/font/google'
+
+//Inter Needs to be called like a function and it accepts an options object
+const inter = Inter({
+    //we take subset property with array determine what alphabet u want like latin english
+    subsets: ['latin'],
+    variable: "--font-inter"
+})
+
 // we accept <Component/> in _app.js and display it using special prop called the children prop in plain javascript
 export default function AppLayout({ children}) {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className={`${inter.variable} font-sans min-h-screen flex flex-col`}>
              <header>header</header>
             <main className="flex-grow bg-[#f7f7f7]">
                 {/* within main element we going to display content of each page */}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import AppLayout from 'src/components/Layout'
 import 'src/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-
+  //App layout wraps all of our component
   return <AppLayout>
     <Component {...pageProps} />
   </AppLayout>
