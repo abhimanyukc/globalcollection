e4f5daf024f35b43bafc402da0f15b6143bfdc1a
Abhimanyu kc <abhidesign88@gmail.com>
2023-12-27 06:47:34 +0545

    displays formatted total that we have in our cart and it updates immediately

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { useShoppingCart } from "use-shopping-cart"
 
 export default function  CartPage()  {
-    const { cartCount , clearCart} = useShoppingCart()
+    const { cartCount ,formattedTotalPrice, clearCart} = useShoppingCart()
 
     return (
         <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
@@ -30,9 +30,19 @@ export default function  CartPage()  {
                 </Link>
                </p>
             </>
-
            )}
-
+          
+          {cartCount > 0  && (
+            <div className="mt-12 space-y-4">
+                <div className="flex flex-col items-end border-t py-4 mt-8">
+                    <p className="text-xl">
+                        Total:{""}
+                    <span className="font-semibold">{formattedTotalPrice}</span>
+                    </p>
+                    
+                </div>
+                </div>
+          )}
         </div>
     )
 }
